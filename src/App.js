3f82ed223f046196f667c9eb582ce12e918252e9
Gustavo DeLlac <gustavodellac@gmail.com>
2023-07-23 18:15:18 -0300

    creacion componentes Pantalla y BotonClear, estilos para cada componente, importacion y integracion de componentes en App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import freeCodeCampLogo from "./img/freecodecamp-logo.png"
 import Boton from './componentes/Boton.jsx';
+import Pantalla from "./componentes/Pantalla.jsx";
+import BotonClear from './componentes/BotonClear.jsx';
 
 function App() {
   return (
@@ -12,18 +14,38 @@ function App() {
           alt="Logo de freeCodeCamp" />
       </div>
       <div className="contenedor-calculadora">
+        <Pantalla />
         <div className='fila'>
           <Boton>1</Boton>{
             //Utilizamos etiqueta del componente Boton de apertura y cierre. Dentro de ella podemos colocar un elemento que
             //sera considerado como children, es decir sera enviado como props.children al componente hijo de App que es Boton y 
             //posteriormente renderizado
           }
+          <Boton>2</Boton>
+          <Boton>3</Boton>
           <Boton>+</Boton>
         </div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
       </div>
     </div>
   );
--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import "../hojas-de-estilo/Boton.css";
+
 function Boton(props){
 
   const esOperador = valor => {
